3c88d7e31d205dd7e9fffb352fa2926673275e26
Sofiia Krytska <krytskasofia@gmail.com>
2024-05-26 12:53:44 +0300

    add class for drawing lines

---
+++ sdmt.lab4/src/main/java/org/example/Line.java
@@ -0,0 +1,15 @@
+package org.example;
+
+import java.awt.*;
+
+public class Line extends Shape{
+
+    public Line(Point startPoint, Point endPoint, Color color) {
+        super(startPoint, endPoint, color);
+    }
+    @Override
+    public void draw(Graphics g) {
+        g.setColor(color);
+        g.drawLine(startPoint.x, startPoint.y, endPoint.x, endPoint.y);
+    }
+}
